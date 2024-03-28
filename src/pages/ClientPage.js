04aa08aaaed810c8355f80254e661a023228e840
anarehder <anapaula.rehder@gmail.com>
2024-03-28 11:42:07 -0300

    feat: graficos valores mean e max - radial bar

--- src/pages/ClientPage.js
+++ src/pages/ClientPage.js
@@ -1,43 +1,69 @@
 import styled from 'styled-components';
+import ReactApexChart from 'react-apexcharts';
+import RadialBarComponent from '../components/RadialBar';
 
 export default function ClientPage() {
+    const data = [{'Cliente':'DEFAULT','Name':'CONTROLLER-OCI-PRIVATE','OCID': 'ocid1.instance.oc1.sa-saopaulo-1.antxeljrr34ngcycmcca6cbkk3ezjbivtznoltngetrcx6aejhvzlypmq2ja','Compartment': 'root','AD': 'SA-SAOPAULO-1-AD-1','Status': 'RUNNING','OS': 'Oracle Linux 7.9','Shape': 'VM.Standard.E4.Flex','OCPU': '2.0','MEMORY': '16.0','Armazenamento': '47','Custo_Atual_de_Maquina':'266.94','Custo_Atual_de_OS': '0','Custo_de_Disco': '10.01','MaxCPU': '87.7','MeanCPU':'57.77','MaxMEM':'39.7','MeanMEM': '36.27'}];
 
     return (
         <PageContainer>
-            PÁGINA DO CLIENTE - DADOS ÚLTIMOS 30 DIAS !
+            <h1>
+                PÁGINA DO CLIENTE - DADOS ÚLTIMOS 30 DIAS ! - TENANCY ACCERTE
+            </h1>
             <CompartmentInfo>
                 <Info>
-                <h2>COMPARTMENTS</h2>
-                <h2>Instance</h2>
-                <h2>OCID</h2>
-                <h2>Compartment</h2>
-                <h2>AD</h2>
-                <h2>Status</h2>
-                <h2>OS</h2>
-                <h2>Armazenamento(GB)</h2>
-            </Info>
+                <div>
+                    <h2>Compartment</h2>
+                    <h3> {data[0]?.Compartment} </h3>
+                </div>
+                <div>
+                    <h2>Instance</h2>
+                    <h3> {data[0]?.AD} </h3>
+                </div>
+                <div>
+                    <h2>OCID</h2>
+                    <h3> {data[0]?.OCID} </h3>
+                </div>
+                <div>
+                    <h2>Status</h2>
+                    <h3> {data[0]?.Status} </h3>
+                </div>
+                <div>
+                    <h2>OS</h2>
+                    <h3> {data[0]?.OS} </h3>
+                </div>
+                <div>
+                    <h2>Armazenamento(GB)</h2>
+                    <h3> {data[0]?.Armazenamento} </h3>
+                </div>
+            </Info> 
             <OCPU_MEM>
-                <h1>OCPU</h1>
-                <h2>OCPU, MaxCPU(%), MeanCPU(%)</h2>
-                <h2>newOCPU</h2>
+                <h1>OCPU: {data[0]?.OCPU}</h1>
+                <h2>Sugestão newOCPU: XX</h2>
+                <div>
+                    <RadialBarComponent value = {data[0]?.MeanCPU} name = {'MeanCPU'} />
+                    <RadialBarComponent value = {data[0]?.MaxCPU} name = {'MaxCPU'} />
+                </div>
             </OCPU_MEM>
             <OCPU_MEM>
-                <h1>MEMORY</h1>
-                <h2>MEMORY(GB), MaxMEM(%), MeanMEM(%)</h2>
-                <h2>newMEM</h2>
+                <h1>MEMORY: {data[0]?.MEMORY} GB</h1>
+                <h2>Sugestão newMEM: XX</h2>
+                <div>
+                    <RadialBarComponent value = {data[0]?.MeanMEM} name = {'MeanMEM'} />
+                    <RadialBarComponent value = {data[0]?.MaxMEM} name = {'MaxMEM'} />
+                </div>
             </OCPU_MEM>
             <Prices>
                 <ShapeInfo>
-                <h2>Custo Atual de Maquina(24x7)</h2>
-                <h2>Custo Atual de OS(24x7)</h2>
-                <h2>Custo de Disco</h2>
+                <h1>Custo Atual de Maquina(24x7): R$ {data[0]?.Custo_Atual_de_Maquina}</h1>
+                <h2>Custo Atual de OS(24x7): R$ {data[0]?.Custo_Atual_de_OS}</h2>
+                <h2>Custo de Disco: R$ {data[0]?.Custo_de_Disco}</h2>
                 </ShapeInfo>
                 <ShapeInfo>
-                <h2>Ajuste</h2>
-                <h2>Tipo de Operacao</h2>
-                <h2>Custo Estimado (Sem alteracao de shape)</h2>
-                <h2>Recomendacao Accerte (Shape E5.Flex)</h2>
-                <h2>Novo Custo de OS (24x7)</h2>
+                <h2>Ajuste / Tipo de Operacao</h2>
+                <h1>Custo Estimado (Sem alteracao de shape): R$ XX.XX</h1>
+                <h1>Recomendacao Accerte (Shape E5.Flex): R$ XX.XX</h1>
+                <h2>Novo Custo de OS (24x7): R$ XX.XX</h2>
                 </ShapeInfo>
             </Prices>
             </CompartmentInfo>
@@ -55,38 +81,52 @@ const PageContainer = styled.div`
 `
 
 const CompartmentInfo = styled.div`
-    justify-content: flex-start !important;
-    gap: 10px;
+    gap: 20px;
     flex-wrap: wrap;
+    border: 1px solid gray;
 `
 
 const Info = styled.div`
     justify-content: flex-start !important;
-    gap: 40px;
+    div {
+        flex-direction: column;
+        gap: 10px;
+        text-align: center;
+        font-size: 19px;
+        font-weight: 400;
+    }
 `
 
 const OCPU_MEM = styled.div`
-    background-color: gray;
     flex-direction: column;
-    justify-content: flex-start !important;
-    gap: 40px;
+    justify-content: space-between !important;
     max-width: 30%;
-    height: 250px;
+    border: 1px solid gray;
+    height: 275px;
+    div {
+        padding: 0 !important;
+    }
 `
 
 const Prices = styled.div`
-    background-color: green;
     flex-direction: column;
     max-width: 30%;
     gap: 10px;
     flex-wrap: wrap;
-    height: 250px;
+    height: 275px;
+    border: 1px solid gray;
 `
 
 const ShapeInfo = styled.div`
     justify-content: flex-start !important;
     flex-wrap: wrap;
     gap: 10px;
-    border: 1px solid black;
+    h2{
+        width: 80%;
+        font-size: 17px;
+    }
+    h1 {
+        font-size: 18px;
+    }
 `
 
