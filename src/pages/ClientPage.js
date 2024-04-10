fc88918b888b937b46eec65a14a1e3ceff35f502
anarehder <anapaula.rehder@gmail.com>
2024-04-10 14:50:59 -0300

    fix: remocao array data client page

--- src/pages/ClientPage.js
+++ src/pages/ClientPage.js
@@ -6,8 +6,7 @@ import { Link } from 'react-router-dom';
 
 export default function ClientPage() {
     const [machines, setMachines] = useState([]);
-    const data = [{'Cliente':'DEFAULT','Name':'CONTROLLER-OCI-PRIVATE','OCID': 'ocid1.instance.oc1.sa-saopaulo-1.antxeljrr34ngcycmcca6cbkk3ezjbivtznoltngetrcx6aejhvzlypmq2ja','Compartment': 'root','AD': 'SA-SAOPAULO-1-AD-1','Status': 'RUNNING','OS': 'Oracle Linux 7.9','Shape': 'VM.Standard.E4.Flex','OCPU': '2.0','MEMORY': '16.0','Armazenamento': '47','Custo_Atual_de_Maquina':'266.94','Custo_Atual_de_OS': '0','Custo_de_Disco': '10.01','MaxCPU': '87.7','MeanCPU':'57.77','MaxMEM':'39.7','MeanMEM': '36.27'}];
-
+    
     useEffect(() => {
         const fetchData = async () => {
             try {
